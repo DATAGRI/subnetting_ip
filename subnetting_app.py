dd162eaa8a9f89b32d3c7bb13e918d49607d238c
irwn93 <irwn1993@gmail.com>
2018-05-06 19:50:30 -0500

    funcion conversion binario a digital agregada

--- subnetting_app.py
+++ subnetting_app.py
@@ -15,9 +15,59 @@ def dig_bin ():
     bin1.reverse()
     print(bin1)
 
-def bina_digi ():
-    pass
+def bin_digi ():
+    bin2 = [[128], [64],[32],[16],[8],[4],[2],[1]]
+    bina = []
+
+    i = 8
+    while i > 0:
+        i = i - 1
+        num = int(input('ingrese numero binario'))
+        if num == 1 or num == 0:
+            bina.append(num)
+    
+    if bina[0] == 1:
+        d1 = bin2[0][0]
+    else: 
+        d1 = 0
+
+    if bina[1] == 1:
+        d2 = bin2[1][0]
+    else:
+        d2 = 0
+    
+    if bina[2] == 1:
+        d3 = bin2[2][0]
+    else: 
+        d3 = 0
+
+    if bina[3] == 1:
+        d4 = bin2[3][0]
+    else:
+        d4 = 0
+
+    if bina[4] == 1:
+        d5 = bin2[4][0]
+    else: 
+        d5 = 0
+
+    if bina[5] == 1:
+        d6 = bin2[5][0]
+    else:
+        d6 = 0
+    if bina[6] == 1:
+        d7 = bin2[6][0]
+    else: 
+        d7 = 0
+
+    if bina[7] == 1:
+        d8 = bin2[7][0]
+    else:
+        d8 = 0
+
+    sum_dec = d1 + d2 + d3 +d4 +d5 +d6 + d7 + d8
+    print(sum_dec)
 
 
-dig_bin()
-#bin_dig()
+#dig_bin()
+bin_digi()
